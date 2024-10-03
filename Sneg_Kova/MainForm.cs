901fcd92cd53906e57f5ddd958dc556c054b56e1
Korobeynikova <Korobeynikova@B329-L01>
2024-10-03 09:37:52 +0300

    SnowFlakeAddClass

--- Sneg_Kova/MainForm.cs
+++ Sneg_Kova/MainForm.cs
@@ -12,16 +12,30 @@ namespace Sneg_Kova
 {
     public partial class MainForm : Form
     {
+        private Snowflake[] snowflakes;
         public MainForm()
         {
             InitializeComponent();
+            InitializeSnowflakes();
             TimerInitialize();
+        }
+
+        private void InitializeSnowflakes()
+        {
+            const int initialCount = 50;
+            var snowflakes = new Snowflake[initialCount];
+            for (int i = 0; i < initialCount; i++)
+            {
+                float weight = new Random().Next(1, 10); // Вес снежинка от 1 до 10
+                snowflakes[i] = new Snowflake(weight);
+            }
+            this.snowflakes = snowflakes;
         }
         private void TimerInitialize()
         {
             Timer timerSnow = new Timer
             {
-                Interval = 80
+                Interval = 16
             };
             timerSnow.Tick += timerSnow_Tick;
             timerSnow.Start();
@@ -29,7 +43,11 @@ namespace Sneg_Kova
 
         private void timerSnow_Tick(object sender, EventArgs e)
         {
-
+            foreach (var snowflake in snowflakes)
+            {
+                snowflake.UpdatePosition();
+            }
+            Invalidate();
         }
     }
 }
